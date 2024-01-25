87edce2a0627c94e324c441fa10b3a655bbbfd88
WendyChen2022 <qiwenchen529@gmail.com>
2024-01-25 13:37:14 -0800

    Qiwen made some changes

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+// Food constructor
+function Food(pName, pCalories) {
+    this.name = pName;
+    this.calories = parseInt(pCalories);
+}
+
+// Array to store food items
+const foodArray = [];
+
+// Function to add food to the array
+function addFood() {
+    const foodName = document.getElementById("foodName").value;
+    const calories = document.getElementById("calories").value;
+
+    if (foodName && calories) {
+        const newFood = new Food(foodName, calories);
+        foodArray.push(newFood);
+
+        // Update display in the console
+        console.log("Updated Food List:", foodArray);
+
+        // Update display on the screen
+        //displayFoodList();
+        displayTotalCalories();
+    }
+}
+
+
+
+// Function to display total calories on the screen
+function displayTotalCalories() {
+    const totalCaloriesDiv = document.getElementById("totalCalories");
+    const totalCalories = foodArray.reduce((total, food) => total + food.calories, 0);
+    totalCaloriesDiv.textContent = `Total Calories: ${totalCalories}`;
+}
 
